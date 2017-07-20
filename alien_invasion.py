aea85cd7d0e9df227139d82384108c24969d6773
117542544 <117542544@qq.com>
2017-07-20 09:52:09 +0800

    201707200951:13.5_ends

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,7 +11,7 @@ def run_game():
     # 初始化pygame、设置屏幕对象
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_hight))
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     # 创建一艘飞船，一个子弹编组和一个外星人编组
@@ -20,13 +20,14 @@ def run_game():
     aliens = Group()
 
     # 创建外星人群
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # 开始游戏主循环
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_bullets(bullets)
+        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+        gf.update_aliens(ai_settings, aliens)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
